91aae4c8279daa099b725f8aa9f587a7f5cdc757
ry <renyanstone@163.com>
2019-05-27 23:02:12 +0800

    commit the result of taxi

--- main/main.js
+++ main/main.js
@@ -1,4 +1,6 @@
-module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
-};
+module.exports = function main(inputs) { 
+   var pt =inputs.parkTime;
+   var dis = inputs.distance;
+   var price = Math.round(6+(pt>0?pt*0.25:0)+(dis>2?(dis>8?(dis-2)*0.8+(dis-8)*0.4:(dis-2)*0.8):0));
+   return price; 
+}
